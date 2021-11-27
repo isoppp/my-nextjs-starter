46e1879fb45f3223ade93242f1ad4137cc2d3fec
isoppp <prog@d2deck.com>
2021-11-27 10:16:05 +0900

    enable jit mode and update tailwind config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import { useFlashMessageDispatcher } from '@/hooks/useFlashMessageDispatcher'
 import { NextPage } from 'next'
+
 const IndexPage: NextPage = () => {
   const { addSuccessMessage, addErrorMessage } = useFlashMessageDispatcher()
   return (
     <div>
-      <h1>Home</h1>
-      <div className="flex gap-4">
-        <button onClick={() => addSuccessMessage('Success!')}>Add Message</button>
-        <button onClick={() => addErrorMessage('Error!')}>Add Error</button>
+      <h1 className="mb-[10px]">Home</h1>
+      <div className="flex">
+        <div className="ml-4 first:ml-0">
+          <button onClick={() => addSuccessMessage('Success!')}>Add Message</button>
+        </div>
+        <div className="ml-4 first:ml-0">
+          <button onClick={() => addErrorMessage('Error!')}>Add Error</button>
+        </div>
       </div>
     </div>
   )
