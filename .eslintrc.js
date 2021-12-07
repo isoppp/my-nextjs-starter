0d7b8c7d43da55e79ca93d792bfc5ce097f6271d
isoppp <prog@d2deck.com>
2021-12-08 01:46:03 +0900

    adjust eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,15 +37,6 @@ module.exports = {
 
         // Why would you want unused vars?
         '@typescript-eslint/no-unused-vars': ['error'],
-
-        // I suggest this setting for requiring return types on functions only where useful
-        '@typescript-eslint/explicit-function-return-type': [
-          'warn',
-          {
-            allowExpressions: true,
-            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-          },
-        ],
       },
     },
   ],
