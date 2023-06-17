381b33eae841feafae35ba6349d6837621ae144d
isoppp <prog@d2deck.com>
2023-06-17 21:15:45 +0900

    fix type error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,10 @@ const IndexPage: NextPage = () => {
       <h1 className="mb-[10px]">Home</h1>
       <div className="flex">
         <div className="ml-4 first:ml-0">
-          <button onClick={() => addSuccessMessage('Success!')}>Add Message</button>
+          <button onClick={() => addSuccessMessage?.('Success!')}>Add Message</button>
         </div>
         <div className="ml-4 first:ml-0">
-          <button onClick={() => addErrorMessage('Error!')}>Add Error</button>
+          <button onClick={() => addErrorMessage?.('Error!')}>Add Error</button>
         </div>
       </div>
     </div>
